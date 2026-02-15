52be8aa31099c2a20e4d1bb81daf0c6ac75c765a
udamendis <udamendis@gmail.com>
2026-02-15 08:37:40 +0100

    Updated to the simulation engine

--- pyspice/fdda_simulator.py
+++ pyspice/fdda_simulator.py
@@ -43,6 +43,11 @@ def generate_spice_file(params):
 
     content = f"""* FDDA (Auto-generated)
 .subckt FDDA VDD Vpp Vpn Vnp Vnn Vcmfb Vb1 Vb2 Vb3 Vb4 Von Vop
+Vmeas1 net11 net1 0V
+Vmeas2 net5 net9 0V
+Vmeas3 net6 net10 0V
+Vmeas4 net12 net2 0V
+Vmeas5 net13 net9 0V
 XMP1 net5 Vpp net1 VDD sky130_fd_pr__pfet_01v8 L={params['L_1']} {g_1} sa=0 sb=0 sd=0
 + mult=1 m=1
 XMP2 net6 Vpn net1 VDD sky130_fd_pr__pfet_01v8 L={params['L_1']} {g_1} sa=0 sb=0 sd=0
@@ -59,17 +64,17 @@ XMP7 Von Vb4 net3 VDD sky130_fd_pr__pfet_01v8 L={params['L_3']} {g_3} sa=0 sb=0 
 + mult=1 m=1
 XMP8 Vop Vb4 net4 VDD sky130_fd_pr__pfet_01v8 L={params['L_3']} {g_3} sa=0 sb=0 sd=0
 + mult=1 m=1
-XMP11 net1 Vb1 VDD VDD sky130_fd_pr__pfet_01v8 L={params['L_6']} {g_6} sa=0 sb=0 sd=0
+XMP11 net11 Vb1 VDD VDD sky130_fd_pr__pfet_01v8 L={params['L_6']} {g_6} sa=0 sb=0 sd=0
 + mult=1 m=1
-XMP12 net2 Vb1 VDD VDD sky130_fd_pr__pfet_01v8 L={params['L_6']} {g_6} sa=0 sb=0 sd=0
+XMP12 net12 Vb1 VDD VDD sky130_fd_pr__pfet_01v8 L={params['L_6']} {g_6} sa=0 sb=0 sd=0
 + mult=1 m=1
-XMN3 Von Vb3 net5 0 sky130_fd_pr__nfet_01v8 L={params['L_4']} {g_4} sa=0 sb=0 sd=0
+XMN3 Von Vb3 net13 0 sky130_fd_pr__nfet_01v8 L={params['L_4']} {g_4} sa=0 sb=0 sd=0
 + mult=1 m=1
 XMN4 Vop Vb3 net6 0 sky130_fd_pr__nfet_01v8 L={params['L_4']} {g_4} sa=0 sb=0 sd=0
 + mult=1 m=1
-XMN9 net5 Vb2 0 0 sky130_fd_pr__nfet_01v8 L={params['L_5']} {g_5} sa=0 sb=0 sd=0
+XMN9 net9 Vb2 0 0 sky130_fd_pr__nfet_01v8 L={params['L_5']} {g_5} sa=0 sb=0 sd=0
 + mult=1 m=1
-XMN10 net6 Vb2 0 0 sky130_fd_pr__nfet_01v8 L={params['L_5']} {g_5} sa=0 sb=0 sd=0
+XMN10 net10 Vb2 0 0 sky130_fd_pr__nfet_01v8 L={params['L_5']} {g_5} sa=0 sb=0 sd=0
 + mult=1 m=1
 
 .ends
@@ -117,14 +122,39 @@ XMN8 nt2 Vb2 0 0 sky130_fd_pr__nfet_01v8 L={params['L_5']} {g_5} sa=0 sb=0 sd=0
     with open('cmfb.spice', 'w') as f:
         f.write(content)
 
+def generate_bias():
+
+    content=f"""*Bias circuit auto generated
+.subckt BIAS VDD Vb1 Vb2 Vb3 Vb4
+
+IREF VDD n1 2.5uA
+XMN11 n1 n1 0 0 sky130_fd_pr__nfet_01v8 L=2 W=5
+XMN12 Vb1 n1 0 0 sky130_fd_pr__nfet_01v8 L=2 W=5
+
+XMP14 Vb1 Vb1 VDD VDD sky130_fd_pr__pfet_01v8 L=2 W=35
+XMP15 Vb3 Vb1 VDD VDD sky130_fd_pr__pfet_01v8 L=2 W=35
+XMP16 Vb2 Vb1 VDD VDD sky130_fd_pr__pfet_01v8 L=2 W=35
+XMP17 Vb4 Vb4 VDD VDD sky130_fd_pr__pfet_01v8 L=3 W=5
+
+XMN13 Vb3 Vb3 0 0 sky130_fd_pr__nfet_01v8 L=4 W=1
+XMN14 Vb2 Vb2 0 0 sky130_fd_pr__nfet_01v8 L=2 W=5
+XMN15 Vb4 Vb2 0 0 sky130_fd_pr__nfet_01v8 L=2 W=5
+
+.ends BIAS
+"""
+
+    with open('bias.spice', 'w') as f:
+        f.write(content)
+
 
 def run_simulation(mode):
 
     circuit = Circuit('FDDA Simulation')
     circuit.raw_spice ="""
 .control
-set no_warning
-set no_note
+    set no_warning
+    set no_note
+    save all
 .endc
 
 .options ngbehavior=hsa
@@ -136,6 +166,8 @@ set no_note
 .lib /foss/pdks/sky130A/libs.tech/ngspice/sky130.lib.spice tt
 .include /foss/designs/os-ina-pso-mixdes_2026/pyspice/fdda.spice
 .include /foss/designs/os-ina-pso-mixdes_2026/pyspice/cmfb.spice
+.include /foss/designs/os-ina-pso-mixdes_2026/pyspice/bias.spice
+
 """
     # pdk_path = '/foss/pdks/sky130A/libs.tech/ngspice/sky130.lib.spice'
     # circuit.lib('/foss/pdks/sky130A/libs.tech/ngspice/sky130.lib.spice', 'tt')
@@ -143,29 +175,33 @@ set no_note
 
     circuit.X('X1', 'FDDA', 'VDD', 'Vpp', 'Vpn', 'Vnp', 'Vnn', 'Vcmfb', 'Vb1', 'Vb2', 'Vb3', 'Vb4', 'Von', 'Vop')
     circuit.X('X2', 'CMFB', 'VDD', 'Vop', 'Von', 'Vcm', 'Vb2', 'Vcmfb')
+    circuit.X('X3', 'BIAS', 'VDD', 'Vb1', 'Vb2', 'Vb3', 'Vb4')
     
     circuit.V('vdd', 'VDD', circuit.gnd, 1.8@u_V)
-    circuit.V('vb1', 'Vb1', circuit.gnd, 0.776@u_V)
-    circuit.V('vb2', 'Vb2', circuit.gnd, 0.665@u_V)
-    circuit.V('vb3', 'Vb3', circuit.gnd, 0.898@u_V)
-    circuit.V('vb4', 'Vb4', circuit.gnd, 0.579@u_V)
+    # circuit.V('vb1', 'Vb1', circuit.gnd, 0.776@u_V)
+    # circuit.V('vb2', 'Vb2', circuit.gnd, 0.665@u_V)
+    # circuit.V('vb3', 'Vb3', circuit.gnd, 0.898@u_V)
+    # circuit.V('vb4', 'Vb4', circuit.gnd, 0.579@u_V)
     circuit.V('vcm', 'Vcm', circuit.gnd, 0.9@u_V)
     # circuit.V('vcmfb', 'Vcmfb', circuit.gnd, 0.76@u_V)
     # circuit.R('rcm1', 'Vop', 'Vcm_sense', 1@u_MOhm)
     # circuit.R('rcm2', 'Von', 'Vcm_sense', 1@u_MOhm)
     # circuit.V('vcm', 'Vcmfb', 'Vcm_sense', 0@u_V)
     
-    circuit.C('load_p', 'Vop', circuit.gnd, 2@u_pF)
-    circuit.C('load_n', 'Von', circuit.gnd, 2@u_pF)
+    circuit.C('load_p', 'Vop', circuit.gnd, 0.5@u_pF)
+    circuit.C('load_n', 'Von', circuit.gnd, 0.5@u_pF)
     
 
 
 
     if mode == 'AC':        # Define AC Input (Differential)
-        circuit.V('vpp', 'Vpp', circuit.gnd, 'dc 0.9 ac 0.5')
-        circuit.V('vpn', 'Vpn', circuit.gnd, 'dc 0.9 ac -0.5')
-        circuit.V('vnp', 'Vnp', circuit.gnd, 'dc 0.9 ac -0.5')
-        circuit.V('vnn', 'Vnn', circuit.gnd, 'dc 0.9 ac 0.5')
+        circuit.V('vpp', 'Vpp', circuit.gnd, 'dc 0 ac 0.5')
+        circuit.V('vpn', 'Vpn', circuit.gnd, 'dc 0 ac -0.5')
+        circuit.V('vnp', 'Vnp', circuit.gnd, 'dc 0 ac -0.5')
+        circuit.V('vnn', 'Vnn', circuit.gnd, 'dc 0 ac 0.5')
+        # circuit.R('rnnop', 'Vnn', 'Vop', 10@u_kOhm)
+        # circuit.R('ropon', 'Vop', 'Von', 60@u_kOhm)
+        # circuit.R('ronnp', 'Von', 'Vnp', 10@u_kOhm)
 
         simulator = circuit.simulator(simulator='ngspice-subprocess', temperature=25)
 
@@ -178,29 +214,88 @@ set no_note
             raise e
         # .options noopiter
         op_analysis = simulator.operating_point()
-        # i_total = abs(float(op_analysis.branches['vvdd'][0]))
-        # power_uw = i_total * 1.8 * 1e6  # Power = VDD * I_total
 
         # Check DC levels before looking at AC results
         print(f"DC Vop: {float(op_analysis.nodes['Vop'][0]):.3f} V")
         print(f"DC Von: {float(op_analysis.nodes['Von'][0]):.3f} V")
         print(f"DC Vcmfb: {float(op_analysis.nodes['Vcmfb'][0]):.3f} V")
+        print(f"net1: {float(op_analysis.nodes['xx1.net1'][0]):.3f} V")
+        print(f"net2: {float(op_analysis.nodes['xx1.net2'][0]):.3f} V")
+        print(f"Vb1: {float(op_analysis.nodes['Vb1'][0]):.3f} V")
+        print(f"Vb2: {float(op_analysis.nodes['Vb2'][0]):.3f} V")
+        print(f"Vb3: {float(op_analysis.nodes['Vb3'][0]):.3f} V")
+        print(f"Vb4: {float(op_analysis.nodes['Vb4'][0]):.3f} V")
+
+        print(f"Current net11: {float(op_analysis.branches['v.xx1.vmeas1'][0])*1e6:.3f} uA")
+        print(f"Current net12: {float(op_analysis.branches['v.xx1.vmeas4'][0])*1e6:.3f} uA")
+        print(f"Current net13: {float(op_analysis.branches['v.xx1.vmeas5'][0])*1e6:.3f} uA")
+        print(f"Current net5: {float(op_analysis.branches['v.xx1.vmeas2'][0])*1e6:.3f} uA")
+        print(f"Current net6: {float(op_analysis.branches['v.xx1.vmeas3'][0])*1e6:.3f} uA")
+
+        with open('simulation_results.txt', 'w') as f:
+            f.write("--- NODE VOLTAGES AT START FREQUENCY ---\n")
+            f.write(f"{'Node Name':<20} | {'Voltage (V)':<10}\n")
+            f.write("-" * 35 + "\n")
+
+            for node_name in op_analysis.nodes:
+                # Get the complex value and print magnitude
+                voltage_complex = op_analysis.nodes[node_name][0]
+                magnitude = float(np.abs(voltage_complex))
+                f.write(f"{node_name:<20} | {magnitude:.6f} V\n")
 
         freq = np.array(analysis.frequency)
         vout = analysis.nodes['Vop'] - analysis.nodes['Von']
-        vin  = (analysis.nodes['Vpp'] - analysis.nodes['Vpn'])
+        vin  = (analysis.nodes['Vpp'] - analysis.nodes['Vpn'])-(analysis.nodes['Vnp'] - analysis.nodes['Vnn'])
 
         gain = vout / vin
-        gain_db = 20 * np.log10(np.abs(gain))
+        gain_db = 20*np.log10(np.abs(gain))
         phase_deg = np.angle(gain, deg=True)
 
-        # gain_db = 20 * np.log10(np.absolute(v_diff))
-        # phase_deg = np.angle(v_diff, deg=True)        # Use deg=True here because built-in bode_diagram expects degrees
-        mag_db = 20 * np.log10(np.absolute(gain))
-        dc_gain = mag_db[0]
-        gbw = np.interp(0, mag_db[::-1], freq[::-1])
+        # mag_db = 20 * np.log10(np.absolute(gain))
+        dc_gain = gain_db[0]
+        gbw = np.interp(0, gain_db[::-1], freq[::-1])
         phase_at_gbw = np.interp(gbw, freq, phase_deg)
         phase_margin = 180 + phase_at_gbw
+
+        # Performing the CMRR A_CM calculation
+        for name in ['Vvpp', 'Vvpn', 'Vvnp', 'Vvnn']:
+            circuit._elements.pop(name)
+
+        circuit.V('vpp', 'Vpp', circuit.gnd, 'dc 0 ac 1')
+        circuit.V('vpn', 'Vpn', circuit.gnd, 'dc 0 ac 1')
+        circuit.V('vnp', 'Vnp', circuit.gnd, 'dc 0 ac 1')
+        circuit.V('vnn', 'Vnn', circuit.gnd, 'dc 0 ac 1')
+        # circuit.R('rnnop', 'Vnn', 'Vop', 10@u_kOhm)
+        # circuit.R('ropon', 'Vop', 'Von', 60@u_kOhm)
+        # circuit.R('ronnp', 'Von', 'Vnp', 10@u_kOhm)
+
+        simulator = circuit.simulator(simulator='ngspice-subprocess', temperature=25)
+
+        try:
+            analysis = simulator.ac(start_frequency=1@u_Hz, stop_frequency=100@u_MHz, number_of_points=20, variation='dec')
+        except Exception as e:
+            print("\n--- NGSPICE ERROR LOG ---")
+            print(str(simulator))
+            print(e)
+            raise e
+
+        vout_cm = analysis.nodes['Vop'] - analysis.nodes['Von']
+        vin_cm  = (analysis.nodes['Vpp'] + analysis.nodes['Vpn'] + analysis.nodes['Vnp'] + analysis.nodes['Vnn'])/4
+
+        gain_cm = vout_cm / vin_cm
+        # gain_cm = vout_cm
+        gain_cm_db = 20 * np.log10(np.abs(gain_cm))
+
+        # finding the gain at the target freq of 1kHz
+        target_freq = 1000  # 1kHz
+        idx = (np.abs(freq - target_freq)).argmin()
+
+        ad_1k = (gain_db[idx])
+        acm_1k = (gain_cm_db[idx])
+        print(f"Calculated AC Gain @1kHz: {ad_1k:.2f} dB")
+        print(f"Calculated CM Gain @1kHz: {acm_1k:.2f} dB")
+
+        cmrr = ad_1k - acm_1k
 
         print(f"\n--- AC ANALYSIS RESULTS ---")
         print(f"Calculated DC Gain: {dc_gain:.2f} dB | Calculated GBW: {gbw/1e6:.2f} MHz | Calculated PM: {phase_margin:.2f} degrees")
@@ -209,11 +304,12 @@ set no_note
         specs = {
             "Open-loop gain": {"val": dc_gain, "target": 70, "unit": "dB", "op": ">="},
             "GBW": {"val": gbw/1e6, "target": 30, "unit": "MHz", "op": ">="},
-            "Phase Margin": {"val": phase_margin, "target": 60, "unit": "deg", "op": ">"}
+            "Phase Margin": {"val": phase_margin, "target": 60, "unit": "deg", "op": ">"},
+            "CMRR": {"val": cmrr, "target": 120, "unit": "db", "op": ">"}
             # "Power Total": {"val": power_uw, "target": 500, "unit": "uW", "op": "<="}
         }
 
-        print(f"\n--- Results for L={params['L_1']}, W={params['W_1']} ---")
+        print(f"\n--- Results ---")
         all_passed = True
         
         fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 8), sharex=True)
@@ -223,13 +319,19 @@ set no_note
         ax1.grid(True, which="both", ls="-")
         ax1.set_title("Bode Diagram of Biomedical INA")
 
-        ax2.semilogx(freq, phase_deg, color='blue', marker='.', linestyle='-')
-        ax2.set_ylabel('Phase (Degrees)')
+        ax2.semilogx(freq, gain_cm_db, color='red', marker='.', linestyle='-')
+        ax2.set_ylabel('Gain cm (db)')
         ax2.set_xlabel('Frequency (Hz)')
         ax2.grid(True, which="both", ls="-")
 
-        ax1.axhline(y=0, color='red', linestyle='--') 
-        ax2.axhline(y=-180, color='red', linestyle='--') 
+        # ax3.semilogx(freq, phase_margin, color='green', marker='.', linestyle='-')
+        # ax3.set_ylabel('Phase')
+        # ax3.set_xlabel('Frequency (Hz)')
+        # ax3.grid(True, which="both", ls="-")
+
+        ax1.axhline(y=0, color='red', linestyle='--')
+        ax2.axhline(y=0, color='red', linestyle='--')  
+        # ax3.axhline(y=-180, color='red', linestyle='--') 
 
         plt.savefig('Gain_and_GBW_plot.png')
         plt.tight_layout()
@@ -271,20 +373,15 @@ set no_note
         print(f"I-Quiescent : {abs(iq)*1e6:.2f} uA")
 
     elif mode == 'TRANS':
-        # 1. Define Differential Inputs (Pair 1 active, Pair 2 grounded at DC)
-        # Using 10mV amplitude to see a clear output without railing 1.8V
         circuit.V('vpp', 'Vpp', circuit.gnd, 'SIN(0.9V 10mV 100Hz)') 
         circuit.V('vpn', 'Vpn', circuit.gnd, 'SIN(0.9V -10mV 100Hz)')
         
-        # Pair 2 stays at Common Mode
         circuit.V('vnp', 'Vnp', circuit.gnd, '0.9V')
         circuit.V('vnn', 'Vnn', circuit.gnd, '0.9V')
 
         simulator = circuit.simulator(simulator='ngspice-subprocess')
-        # Simulate for 3 full cycles of 100Hz (30ms)
         analysis = simulator.transient(step_time=50@u_us, end_time=30@u_ms)
 
-        # 2. Extract results using lowercase node names
         time = np.array(analysis.time)
         vop = np.array(analysis.nodes['Vop'])
         von = np.array(analysis.nodes['Von'])
@@ -330,20 +427,21 @@ set no_note
         plt.show()
 
 params = {
-    'W_1': 1, 'L_1': 1, 'nf_1': 1, 'm_1': 88,
-    'W_2': 1, 'L_2': 1, 'nf_2': 1, 'm_2': 88,
-    'W_3': 1, 'L_3': 1, 'nf_3': 1, 'm_3': 88,
-    'W_4': 1, 'L_4': 1, 'nf_4': 1, 'm_4': 14,
-    'W_5': 1, 'L_5': 1, 'nf_5': 1, 'm_5': 28,
-    'W_6': 1, 'L_6': 1, 'nf_6': 1, 'm_6': 176,
+    'W_1': 88, 'L_1': 1, 'nf_1': 1, 'm_1': 1,
+    'W_2': 88, 'L_2': 1, 'nf_2': 1, 'm_2': 1,
+    'W_3': 88, 'L_3': 1, 'nf_3': 1, 'm_3': 1,
+    'W_4': 14, 'L_4': 1, 'nf_4': 1, 'm_4': 1,
+    'W_5': 28, 'L_5': 1, 'nf_5': 1, 'm_5': 1,
+    'W_6': 88, 'L_6': 1, 'nf_6': 1, 'm_6': 2,
 }
 
 generate_spice_file(params)
 generate_cmfb(params)
+generate_bias()
 
 run_simulation('AC')
 # run_simulation('OP')
-run_simulation('TRANS')
+# run_simulation('TRANS')
 # run_simulation('SLEW')
 
 
