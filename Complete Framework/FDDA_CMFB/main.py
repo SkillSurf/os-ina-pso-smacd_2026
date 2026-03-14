34b7a65b37cac09d8a43b9d3fea6a525f96cf6ce
Avishka Herath <achamuditha@gmail.com>
2026-03-15 00:26:20 +0530

    FDDA 20p 60i w/ DB - Run 1

--- Complete Framework/FDDA_CMFB/main.py
+++ Complete Framework/FDDA_CMFB/main.py
@@ -1,5 +1,5 @@
 import os
-import gc
+# import gc
 import time
 import psutil
 import logging
@@ -115,7 +115,7 @@ def main():
     
     for iteration in range(MAX_ITERATIONS):
 
-        gc.collect()  # Explicitly trigger garbage collection to manage memory
+        # gc.collect()  # Explicitly trigger garbage collection to manage memory
         particles, fitness, need_simulator_check = pso.update_swarm(particles, fitness)
         
         print(f"\nIteration {iteration+1}: Updated {len(need_simulator_check)} particles")
